7618990b94116e8f492793e2966bb85e024371ef
ShodiyorShukurov <shodiyorshukurov07@gmail.com>
2024-07-28 13:17:07 +0500

    Corusel img add

--- src/components/corusel/index.jsx
+++ src/components/corusel/index.jsx
@@ -1,6 +1,7 @@
 import { Carousel } from "antd";
-import img1 from "../../assets/images/practice zone.svg.png"
-import img2 from "../../assets/images/har kunlik.png"
+import img1 from "../../assets/images/practicezone.png"
+import img2 from "../../assets/images/harkunlik.png"
+import img3 from "../../assets/images/ielts.png"
 
 const CarouselPage = () => (
   <Carousel autoplay adaptiveHeight={true} dotWidth={32}>
@@ -9,6 +10,9 @@ const CarouselPage = () => (
     </div>
     <div>
       <img className="corusel-item" src={img2} alt="img" />
+    </div>
+    <div>
+      <img className="corusel-item" src={img3} alt="img" />
     </div>
   </Carousel>
 );
