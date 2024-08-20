f1a75707f26571e12391fdbb55951ba8ee39dfb2
ShodiyorShukurov <shodiyorshukurov07@gmail.com>
2024-08-20 21:00:40 +0500

    edit img

--- src/components/corusel/index.jsx
+++ src/components/corusel/index.jsx
@@ -1,24 +1,24 @@
 import { Carousel } from "antd";
 import img from '../../assets/images/sizning natijangiz web.jpg'
-import img1 from "../../assets/images/har kunlik.png.jpg";
-import img2 from "../../assets/images/hard work web cover (2).jpg";
-import img3 from "../../assets/images/1000+ natijalar web.jpg";
-import img4 from "../../assets/images/practice zone web.jpg";
-import img5 from "../../assets/images/dastur web cover.jpg";
+import img1 from "../../assets/images/1000+ natijalar web.jpg";
+import img2 from "../../assets/images/practice zone web.jpg";
+import img3 from "../../assets/images/dastur web cover.jpg";
+import img4 from "../../assets/images/hard work web cover (2).jpg";
+import img5 from "../../assets/images/har kunlik.png.jpg";
 import img6 from "../../assets/images/2 ta ustoz web cover.jpg";
 import mobile from "../../assets/images/sizning natijangiz.jpg"
-import mobile1 from "../../assets/images/web cover phone.jpg";
-import mobile2 from "../../assets/images/hard work web cover phone (2).jpg";
-import mobile3 from "../../assets/images/1000+ natijalar.jpg";
-import mobile4 from "../../assets/images/practice zone phone.png.jpg";
-import mobile5 from "../../assets/images/sifat phone.jpg";
+import mobile1 from "../../assets/images/1000+ natijalar.jpg";
+import mobile2 from "../../assets/images/practice zone phone.png.jpg";
+import mobile3 from "../../assets/images/sifat phone.jpg";
+import mobile4 from "../../assets/images/hard work web cover phone (2).jpg";
+import mobile5 from "../../assets/images/web cover phone.jpg";
 import mobile6 from "../../assets/images/2 ta ustoz web cover phone.jpg";
 
 
 const CarouselPage = () => (
   <>
     <div className="desktop-version">
-      <Carousel autoplay adaptiveHeight={true} dotWidth={32}>
+      <Carousel autoplay>
         <div>
           <img className="corusel-item" src={img} alt="img" />
         </div>
@@ -50,7 +50,7 @@ const CarouselPage = () => (
     </div>
 
     <div className="mobile-version">
-      <Carousel>
+      <Carousel autoplay>
         <div>
           <img className="mobile-corusel-item" src={mobile} alt="img" />
         </div>
